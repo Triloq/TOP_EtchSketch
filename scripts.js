c3b88a320fff66b17015d481d20b3fda54b45c98
Triloq <25014506+Triloq@users.noreply.github.com>
2023-04-19 15:08:53 -0500

    Got dimensions of squares to fit container

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 
-// Set grid size and point to #container
-let numberDivs = 16*16;
+// Set initial grid and point to #container
+let numberDivs = 16;
 const container = document.querySelector("div");
 // initialize grid of squares
 createGrid(numberDivs);
 // Button prompt to get number of squares
 const buttonPrompt = document.querySelector("button.squareInput");
 buttonPrompt.addEventListener("click", function(e) {
-    btnPrompt();
+    numberOfSquares = btnPrompt();
     // point to all the divs in the grid
     let divs = document.querySelectorAll('div.square');
     // reestablish mouseover listener
@@ -24,20 +24,28 @@ divs.forEach(square => {
     
 })
 
+// Create the grid based on the number input by the user
 function createGrid(numberDivs){
+
+    let squareTotal = numberDivs**2;
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
-    for (i = 0; i < numberDivs; i++){
+    for (i = 0; i < squareTotal; i++){
         let div = document.createElement('div');
         div.classList.add('square');
+        //changing the dimensions based on number of divs
+        div.style.flex = (1/numberDivs*100)+"%";
+        //appending the grid
         container.appendChild(div);
     }
 }
 
+// Button prompts the user for number of squares to create
 function btnPrompt(){
     numberDivs = prompt();
     createGrid(numberDivs);
+    return numberDivs;
 }
 
 function mouseoverSet (divs){
